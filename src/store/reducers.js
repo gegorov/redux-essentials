74e9c128d205cb1b8a826bce525e3aaaa3cc5917
gegorov <gegorov@yahoo.com>
2019-08-12 20:30:38 +0300

    chapter 2 challenges added

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,45 +1,77 @@
-import C from '../constants';
+import C from '../constants'
 
-const initialState = {};
+export const goal = (state=10, action) => 
+	(action.type === C.SET_GOAL) ? 
+		parseInt(action.payload) :
+		state
 
-export const goal = (state = initialState, action) => {
-  switch (action.type) {
-    case C.SET_GOAL:
-      return {
+export const skiDay = (state=null, action) => 
+  (action.type === C.ADD_DAY) ?
+  	action.payload :
+  	state
+
+export const errors = (state=[], action) => {
+  switch(action.type) {
+    case C.ADD_ERROR :
+    	return [
         ...state,
-        days: action.payload,
-      }
+        action.payload
+    	]
+    case C.CLEAR_ERROR : 
+      return state.filter((message, i) => i !== action.payload)
+  	default: 
+  		return state
+  }
+}
 
+export const allSkiDays = (state=[], action) => {
+
+  switch(action.type) {
+
+    case C.ADD_DAY : 
+
+      const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
+
+      return (hasDay) ?
+        state :
+        [
+          ...state,
+          skiDay(null, action)
+        ].sort((a, b) => new Date(b.date) - new Date(a.date))
+
+    case C.REMOVE_DAY :
+
+      return state.filter(skiDay => skiDay.date !== action.payload)     
+
+    default:
+      return state
+  }
+
+}
+
+export const fetching = (state = false, action) => {
+  switch (action.type) {
+    case C.FETCH_RESORT_NAMES:
+      return true;
+    case C.CANCEL_FETCHING:
+      return false;
+      case C.CHANGE_SUGGESTIONS:
+        return false;
     default:
       return state;
   }
 }
 
-export const skiDay = (state = initialState, action) => {
+export const suggestions = (state = [], action) => {
   switch (action.type) {
-    case C.ADD_DAY:
+    case C.CLEAR_SUGGESTIONS:
       return {
         ...state,
-        allSkiDays: [...state.allSkiDays, action.payload],
-      }
+        suggestions: [],
+      };
+    case C.CHANGE_SUGGESTIONS:
+      return action.payload;
     default:
       return state;
   }
-}
-
-export const errors = (state = initialState, action) => {
-  switch (action.type) {
-    case C.ADD_ERROR:
-      return {
-        ...state,
-        errors: [...state.errors, ...action.payload],
-      }
-    case C.CLEAR_ERROR:
-      return {
-        ...state,
-        errors: [...action.payload],
-      }
-    default:
-      return state;
-  }
-}
+};
