1e80cbbc6ae1118730a1f0f6c45385312465f937
gegorov <gegorov@yahoo.com>
2019-08-12 20:55:07 +0300

    add chapter 2 ep 7

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,9 +1,10 @@
+import { combineReducers } from 'redux';
 import C from '../constants'
 
 export const goal = (state=10, action) => 
 	(action.type === C.SET_GOAL) ? 
-		parseInt(action.payload) :
-		state
+		 parseInt(action.payload) :
+		 state
 
 export const skiDay = (state=null, action) => 
   (action.type === C.ADD_DAY) ?
@@ -14,8 +15,8 @@ export const errors = (state=[], action) => {
   switch(action.type) {
     case C.ADD_ERROR :
     	return [
-        ...state,
-        action.payload
+         ...state,
+         action.payload
     	]
     case C.CLEAR_ERROR : 
       return state.filter((message, i) => i !== action.payload)
@@ -33,11 +34,11 @@ export const allSkiDays = (state=[], action) => {
       const hasDay = state.some(skiDay => skiDay.date === action.payload.date)
 
       return (hasDay) ?
-        state :
-        [
-          ...state,
-          skiDay(null, action)
-        ].sort((a, b) => new Date(b.date) - new Date(a.date))
+         state :
+         [
+           ...state,
+           skiDay(null, action)
+         ].sort((a, b) => new Date(b.date) - new Date(a.date))
 
     case C.REMOVE_DAY :
 
@@ -49,29 +50,51 @@ export const allSkiDays = (state=[], action) => {
 
 }
 
-export const fetching = (state = false, action) => {
-  switch (action.type) {
-    case C.FETCH_RESORT_NAMES:
-      return true;
-    case C.CANCEL_FETCHING:
-      return false;
-      case C.CHANGE_SUGGESTIONS:
-        return false;
+export const fetching = (state=false, action) => {
+
+  switch(action.type) {
+
+    case C.FETCH_RESORT_NAMES :
+      return true
+
+    case C.CANCEL_FETCHING :
+      return false 
+
+    case C.CHANGE_SUGGESTIONS :
+      return false   
+
     default:
-      return state;
+      return state
   }
+
 }
 
-export const suggestions = (state = [], action) => {
-  switch (action.type) {
-    case C.CLEAR_SUGGESTIONS:
-      return {
-        ...state,
-        suggestions: [],
-      };
-    case C.CHANGE_SUGGESTIONS:
-      return action.payload;
-    default:
-      return state;
+export const suggestions = (state=[], action) => {
+
+  switch(action.type) {
+
+    case C.CLEAR_SUGGESTIONS :
+      return []
+
+    case C.CHANGE_SUGGESTIONS :
+      return action.payload  
+
+    default :
+      return state
   }
-};
+
+}
+
+const resortNames = combineReducers({
+  fetching,
+  suggestions,
+});
+
+const singleReducer = combineReducers({
+  allSkiDays,
+  goal,
+  errors,
+  resortNames,
+});
+
+export default singleReducer;
