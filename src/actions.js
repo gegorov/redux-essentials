e9abb47d42eb4baebda52fc240971ab6ddecceb7
gegorov <gegorov@yahoo.com>
2019-08-13 12:38:39 +0300

    add chapter 5 starter files

--- src/actions.js
+++ src/actions.js
@@ -1,70 +1,79 @@
 import C from './constants'
-import { suggestions } from './store/reducers';
+import fetch from 'isomorphic-fetch'
 
 export function addDay(resort, date, powder=false, backcountry=false) {
 
-	return {
-		type: C.ADD_DAY,
-		payload: {resort,date,powder,backcountry}
-	}
+    return {
+        type: C.ADD_DAY,
+        payload: {resort,date,powder,backcountry}
+    }
 
 }
 
 export const removeDay = function(date) {
 
-	return {
-		type: C.REMOVE_DAY,
-		payload: date
-	}
+    return {
+        type: C.REMOVE_DAY,
+        payload: date
+    }
 
 }
 
 export const setGoal = (goal) => 
-	({
-		type: C.SET_GOAL,
-		payload: goal
-	})
+    ({
+        type: C.SET_GOAL,
+        payload: goal
+    })
 
+export const addError = (message) => 
+   ({
+      type: C.ADD_ERROR,
+      payload: message
+   })
 
-export const addError = (error) => 
-({
-	type: C.ADD_ERROR,
-	payload: error,
-})
+export const clearError = index => 
+    ({
+        type: C.CLEAR_ERROR,
+        payload: index
+    })   
 
-export const clearError = (index) => 
-({
-	type: C.CLEAR_ERROR,
-	payload: index,
-})
-
-export const changeSuggestions = (newSuggestions) =>
-({
-  type: C.CHANGE_SUGGESTIONS,
-  payload: newSuggestions,
-})
-
-export const clearSuggestions = () =>
-({
-  type: C.CLEAR_SUGGESTIONS,
-})
-
-export const suggestResortNames = value => (dispatch) => {
-  dispatch({
-    type: C.FETCH_RESORT_NAMES,
+export const changeSuggestions = suggestions => 
+  ({
+    type: C.CHANGE_SUGGESTIONS,
+    payload: suggestions
   })
 
-  fetch(`http://localhost:3333/resorts/${value}`)
-    .then(data => data.json())
-    .then(suggestions => dispatch({
-      type: C.CHANGE_SUGGESTIONS,
-      payload: suggestions,
-    }))
-    .catch(error => {
-      dispatch(addError(error.message));
-      dispatch({
-        type: C.CANCEL_FETCHING,
-      });
-    });
+export const clearSuggestions = () => 
+    ({
+        type: C.CLEAR_SUGGESTIONS
+    })
+
+export const suggestResortNames = value => dispatch => {
+
+    dispatch({
+        type: C.FETCH_RESORT_NAMES
+    })
+
+    fetch('http://localhost:3333/resorts/' + value)
+        .then(response => response.json())
+        .then(suggestions => {
+
+            dispatch({
+                type: C.CHANGE_SUGGESTIONS,
+                payload: suggestions
+            })
+
+        })
+        .catch(error => {
+
+            dispatch(
+                addError(error.message)
+            )
+
+            dispatch({
+                type: C.CANCEL_FETCHING
+            })
+
+        })
 
 }
