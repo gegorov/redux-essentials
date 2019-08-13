8da0a6402a89a777bc66033a54dc992b4e38784d
gegorov <gegorov@yahoo.com>
2019-08-13 12:34:54 +0300

    add thunks and async actions

--- src/actions.js
+++ src/actions.js
@@ -1,22 +1,70 @@
-import C from './constants';
+import C from './constants'
+import { suggestions } from './store/reducers';
 
-export const addDay = (resort, date, powder = false, backcountry = false) => ({
-  type: C.ADD_DAY,
-  payload: {
-    resort,
-    date,
-    powder,
-    backcountry,
-  }
-});
+export function addDay(resort, date, powder=false, backcountry=false) {
 
-export const removeDay = (date) => ({
-  type: C.REMOVE_DAY,
-  payload: date
+	return {
+		type: C.ADD_DAY,
+		payload: {resort,date,powder,backcountry}
+	}
 
-});
+}
 
-export const setGoal = goal => ({
-  type: C.SET_GOAL,
-  payload: goal,
-}) 
+export const removeDay = function(date) {
+
+	return {
+		type: C.REMOVE_DAY,
+		payload: date
+	}
+
+}
+
+export const setGoal = (goal) => 
+	({
+		type: C.SET_GOAL,
+		payload: goal
+	})
+
+
+export const addError = (error) => 
+({
+	type: C.ADD_ERROR,
+	payload: error,
+})
+
+export const clearError = (index) => 
+({
+	type: C.CLEAR_ERROR,
+	payload: index,
+})
+
+export const changeSuggestions = (newSuggestions) =>
+({
+  type: C.CHANGE_SUGGESTIONS,
+  payload: newSuggestions,
+})
+
+export const clearSuggestions = () =>
+({
+  type: C.CLEAR_SUGGESTIONS,
+})
+
+export const suggestResortNames = value => (dispatch) => {
+  dispatch({
+    type: C.FETCH_RESORT_NAMES,
+  })
+
+  fetch(`http://localhost:3333/resorts/${value}`)
+    .then(data => data.json())
+    .then(suggestions => dispatch({
+      type: C.CHANGE_SUGGESTIONS,
+      payload: suggestions,
+    }))
+    .catch(error => {
+      dispatch(addError(error.message));
+      dispatch({
+        type: C.CANCEL_FETCHING,
+      });
+    });
+
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,37 @@
-import C from '../constants';
-import appReducer from './reducers';
-import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import C from '../constants'
+import appReducer from './reducers'
+import { createStore, applyMiddleware } from 'redux'
 
 const consoleMessages = store => next => action => {
-  let result;
 
-  console.groupCollapsed(`dispatching action => ${action.type}`);
-  console.log('ski days', store.getState().allSkiDays.length);
-  
-  result = next(action);
+	let result
 
-  const { allSkiDays, goal, errors, resortNames } = store.getState();
-  console.log(`
-    ski days: ${allSkiDays.length}
-    goal: ${goal}
-    fetching: ${resortNames.fetching}
-    suggestions: ${resortNames.suggestion}
-    errors: ${errors.length}
-  `)
+	console.groupCollapsed(`dispatching action => ${action.type}`)
+	console.log('ski days', store.getState().allSkiDays.length)
+	result = next(action)
 
-  console.groupEnd();
+	let { allSkiDays, goal, errors, resortNames } = store.getState()
 
-  return result;
+	console.log(`
+
+		ski days: ${allSkiDays.length}
+		goal: ${goal}
+		fetching: ${resortNames.fetching}
+		suggestions: ${resortNames.suggestions}
+		errors: ${errors.length}
+
+	`)
+
+	console.groupEnd()
+
+	return result
+
+}
+
+export default (initialState={}) => {
+	return applyMiddleware(thunk, consoleMessages)(createStore)(appReducer, initialState)
 }
 
 
-export default (initialState = {}) => {
-  return applyMiddleware(consoleMessages)(createStore)(appReducer, initialState);
-}
+
